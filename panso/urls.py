3fe09f0a5593a1aa3db4c837216904e39da79efb
Joakim Hellsén <tlovinator@gmail.com>
2023-12-07 21:42:40 +0100

    Add /testboi view

--- panso/urls.py
+++ panso/urls.py
@@ -1,12 +1,19 @@
 from __future__ import annotations
 
 from django.contrib import admin
-from django.urls import URLResolver, include, path
+from django.http import HttpRequest, JsonResponse
+from django.urls import include, path
 
 from panso.api import api
 
-urlpatterns: list[URLResolver] = [
+
+def testboi(request: HttpRequest) -> JsonResponse:  # noqa: D103, ARG001
+    return JsonResponse({"status": "ok"})
+
+
+urlpatterns: list = [
     path("admin/", admin.site.urls),
+    path("testboi", testboi, name="testboi"),
     path("api/", api.urls),  # type: ignore  # noqa: PGH003
     path("", include("pages.urls")),
     path("webhallen/", include("webhallen.urls")),
