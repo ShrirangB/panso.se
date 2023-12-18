9b303df647350e7dbe466c386fc2c1cbfec297a3
Joakim Hellsén <tlovinator@gmail.com>
2023-12-18 02:44:35 +0100

    Add /api/intel/processors

--- intel/api.py
+++ intel/api.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from django.http import HttpRequest, JsonResponse
 from ninja import Router
 
-from intel.models import ArkFilterData
+from intel.models import ArkFilterData, Processor
 
 router = Router()
 
@@ -20,5 +20,20 @@ def return_filter_data(request: HttpRequest) -> JsonResponse:  # noqa: ARG001
         return JsonResponse(filters.json_data, safe=False)
     except ArkFilterData.DoesNotExist:
         return JsonResponse([], safe=False)
+    except Exception as e:  # noqa: BLE001
+        return JsonResponse({"error": str(e)}, status=500)
+
+
+@router.get(
+    "/processors",
+    summary="Return a list of ids for all processors.",
+    description="Return a list of ids for all processors. You can use this to get the data for all processors with the /processors/{id} endpoint.",  # noqa: E501
+)
+def return_processor_ids(request: HttpRequest) -> JsonResponse:  # noqa: ARG001
+    """Return a list of ids for all processors."""
+    try:
+        processors = Processor.objects.values_list("product_id", "name")
+        processors = [{"id": p[0], "name": p[1]} for p in processors]
+        return JsonResponse(processors, safe=False)
     except Exception as e:  # noqa: BLE001
         return JsonResponse({"error": str(e)}, status=500)
