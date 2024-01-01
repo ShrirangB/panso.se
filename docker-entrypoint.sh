ff6b18c76590b50e1206e2eee3998940f3967c2f
Joakim Hellsén <tlovinator@gmail.com>
2024-01-01 02:02:09 +0100

    Get workers from amount for cores instead of hardcoding and fix real IP

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -31,7 +31,12 @@ echo "Apply database migrations"
 python manage.py migrate
 echo "Apply database migrations done"
 
+
+# https://docs.gunicorn.org/en/stable/design.html#how-many-workers
+num_cores=$(nproc --all)
+workers=$((2 * num_cores + 1))
+
 # 3. Start server
-echo "Starting server"
-gunicorn --workers=9 --bind=0.0.0.0:8000 panso.wsgi:application --log-level=info --access-logfile=- --error-logfile=- --forwarded-allow-ips="172.*" --proxy-allow-from="172.*"
-echo "Bye"
+echo "Starting server with $workers workers"
+gunicorn --workers=$workers --bind=0.0.0.0:8000 panso.wsgi:application --log-level=info --access-logfile=- --error-logfile=- --forwarded-allow-ips="172.*,192.*" --proxy-allow-from="172.*,192.*"
+echo "Bye, love you"
